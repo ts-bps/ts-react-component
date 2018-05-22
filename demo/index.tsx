f326cc00966b9b578f51e54a658390bd23f5ec75
RakanNimer <rakannimer@gmail.com>
2018-05-22 08:21:57 +0300

    Rename demo import.

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,6 +1,6 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
 
-import { Sum } from "../src/main";
+import { Sum } from "../src/";
 
 ReactDOM.render(<Sum a={1} b={2} />, document.getElementById("app"));
